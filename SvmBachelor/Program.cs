cb9c1f7286d2cbb4fc992a48b638a3622a872368
G B <gytero2@gmail.com>
2019-05-19 02:14:57 +0300

    Add project files.

---
+++ SvmBachelor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SvmBachelor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
